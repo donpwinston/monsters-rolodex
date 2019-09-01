67068fab44aac0919883feb2dc6ae7e88551ded8
Don Winston <satchwinston@yahoo.com>
2019-09-01 15:10:30 -0400

    Updates

---
+++ precache-manifest.3b5b26aafb74949fd94485de1a77478f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f79eb2a6c96350307f58a23d9c00545",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bde92c87c773094b02fc",
+    "url": "/monsters-rolodex/static/css/main.c8485885.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "bde92c87c773094b02fc",
+    "url": "/monsters-rolodex/static/js/main.b77a8b4b.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
